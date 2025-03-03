6aae6527f22842f539fb9f9714b70dfd94c5f669
José Luis Olivares <joseluis_503@yahoo.com>
2025-03-03 13:41:58 -0600

    [ADD] Keycloak and home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
   },
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
   }
 ];
